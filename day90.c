24979aabb82b90c20ee6140d8ffc5846f22bb93c
Aryaanjan <aryan.23568@stu.upes.ac.in>
2025-11-25 23:04:29 +0530

    Create day90.c

---
+++ day90.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+enum Gender { MALE, FEMALE, OTHER };
+
+struct Person {
+    char name[50];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+
+    printf("Enter name: ");
+    scanf("%s", p.name);
+
+    printf("Select gender (0 = MALE, 1 = FEMALE, 2 = OTHER): ");
+    scanf("%d", (int*)&p.gender);
+
+    printf("Name: %s\n", p.name);
+    printf("Gender: ");
+
+    if (p.gender == MALE)
+        printf("Male\n");
+    else if (p.gender == FEMALE)
+        printf("Female\n");
+    else if (p.gender == OTHER)
+        printf("Other\n");
+    else
+        printf("Invalid gender\n");
+
+    return 0;
+}
